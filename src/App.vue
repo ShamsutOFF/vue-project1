cb722a89d72d1f22ef5cad8d4fe1e7c759bee526
ShamsutOFF <shamsutoff@yandex.ru>
2026-02-03 08:04:13 +0300

    Сделал хедер

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script setup>
 
 import MainButton from "@/components/MainButton.vue";
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
+  <Header/>
   <main>
     <MainButton>Начать игру</MainButton>
   </main>
 </template>
 
 <style scoped>
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 120px;
+}
 </style>
---
+++ src/components/Score.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import IconHeart from "@/components/Icons/IconHeart.vue";
+</script>
+
+<template>
+  <div class="score_card">
+    <span>100</span>
+    <IconHeart></IconHeart>
+  </div>
+</template>
+
+<style scoped>
+.score_card {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  color: var(--color-blue-button);
+  text-align: center;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 1.2;
+  border-radius: 36px;
+  background-color: var(--color-light-blue);
+  padding: 12px 24px;
+}
+</style>
