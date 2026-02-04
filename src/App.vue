3fcbc499eb0605d19dd5411ebdd03a80010bcf68
ShamsutOFF <shamsutoff@yandex.ru>
2026-02-04 08:47:25 +0300

    7.6. Упражнение - Список данных

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <script setup>
 import Stat from "@/components/Stat.vue";
 import CitySelect from "@/components/CitySelect.vue";
+import {computed, ref} from "vue";
 
-const data = {
-  label: "ВЛАЖНОСТЬ",
-  stat: "90%"
-}
+let savedCity = ref("Moscow");
+let data = ref({
+  humidity: "75%",
+  temperature: "20°C",
+  wind: "10 м/с"
+});
 
-function getCity(city) {
-  console.log(city);
+const dataModified = computed(() => {
+  return [
+    {
+      label: "Влажность",
+      stat: data.value.humidity
+    },
+    {
+      label: "Температура",
+      stat: data.value.temperature
+    },
+    {
+      label: "Ветер",
+      stat: data.value.wind
+    }];
+});
+
+async function getCity(city) {
+  savedCity.value = city;
 }
 
 </script>
 
 <template>
   <main class="main">
-    <Stat v-bind="data"></Stat>
-    <Stat label="ОСАДКИ" stat="0%"></Stat>
+    <div id="city">{{ savedCity }}</div>
+    <Stat v-for="item in dataModified" v-bind="item" :key="item.label"/>
     <CitySelect @select-city="getCity"/>
   </main>
 </template>
