d214111e9e0582780c1c158b1d7ae3ac0fc6a52c
ShamsutOFF <shamsutoff@yandex.ru>
2026-02-05 15:15:45 +0300

    8.10. Упражнение - Карточка погоды

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,8 @@
 import Stat from "@/components/Stat.vue";
 import CitySelect from "@/components/CitySelect.vue";
 import Error from "@/components/Error.vue";
-import { computed, onMounted, ref } from "vue";
-import IconSun from "@/icons/weather/IconSun.vue";
-import IconCloud from "@/icons/weather/IconCloud.vue";
-import IconRain from "@/icons/weather/IconRain.vue";
+import {computed, onMounted, ref} from "vue";
+import DayCard from "@/components/DayCard.vue";
 
 const API_ENDPOINT = "https://api.weatherapi.com/v1";
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -74,7 +72,7 @@ async function getCity(city) {
     savedCity.value = resData.location.name;
 
   } catch (err) {
-    error.value = { error: { code: 1006 } };
+    error.value = {error: {code: 1006}};
     console.error("Ошибка запроса:", err);
   }
 }
@@ -83,9 +81,7 @@ async function getCity(city) {
 <template>
   <main class="main">
     <Error v-if="error" :error="errorDisplay"/>
-    <IconSun />
-    <IconCloud/>
-    <IconRain/>
+    <DayCard :date="new Date()" temperature="30" weather-code="1000"/>
     <div v-if="!error" id="city">{{ savedCity }}</div>
     <!-- dataModified всегда будет массивом -->
     <Stat v-for="item in dataModified" v-bind="item" :key="item.label"/>
---
+++ src/components/DayCard.vue
@@ -0,0 +1,65 @@
+<script setup>
+import IconSun from "@/icons/weather/IconSun.vue";
+import IconRain from "@/icons/weather/IconRain.vue";
+import IconCloud from "@/icons/weather/IconCloud.vue";
+
+const {weatherCode, temperature, date} = defineProps({
+  weatherCode: {
+    type: String,
+    required: true
+  },
+  temperature: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <button class="day-card">
+    <IconSun v-if="weatherCode == 1000"/>
+    <IconCloud v-if="weatherCode == 1003"/>
+    <IconRain v-if="weatherCode == 1009"/>
+    <div class="day-card__day">
+      {{ date.toLocaleString('ru-RU', {weekday: 'short'}) }}
+    </div>
+    <div class="day-card__temp">
+      {{ temperature + "°C" }}
+    </div>
+  </button>
+</template>
+
+<style scoped>
+.day-card {
+  background-color: var(--color-bg-card);
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  padding: 20px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  color: var(--color-primary);
+  box-shadow: 1px 2px 4px 0 #222831;
+}
+
+.day-card:hover {
+  background-color: var(--color-bg-card-hover);
+}
+
+.day-card__day {
+  font-size: 20px;
+  font-weight: 400;
+}
+
+.day-card__temp {
+  font-size: 20px;
+  font-weight: 700;
+}
+</style>
